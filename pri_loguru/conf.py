8ad0893d7fa3b7c0828443cfb620ce5cadc4e13d
lnk2005 <wza82877638@gmail.com>
2023-12-15 11:43:22 +0800

    chore(handler): update conf

--- pri_loguru/conf.py
+++ pri_loguru/conf.py
@@ -10,14 +10,13 @@ class HandlerConfig(ABC):
 
 @dataclass
 class EsHandlersConfig(HandlerConfig, ABC):
-    use_https: bool
-
     host: str | List[str]
     author: str
     passwd: str
 
-    cert_path: str
-    verify_certs: bool
-
     index: str = f'simple_log_{datetime.now().strftime("%Y%m%d")}'
     queue_size: int = 1  # 一次性提交的日志条数
+
+    use_https: bool = False
+    cert_path: str = ''
+    verify_certs: bool = False
